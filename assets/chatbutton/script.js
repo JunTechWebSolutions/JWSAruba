4af09221673f61db2069f2052e419daf2500e990
Juny Engelhart (AWS Aruba) <juntechwebsolutions@gmail.com>
2025-11-13 21:59:30 -0800

    AWS Aruba rev 01-25

---
+++ assets/chatbutton/script.js
@@ -0,0 +1,3 @@
+
+document.addEventListener("DOMContentLoaded",function(){var a=document.querySelector("#chatbutton-wa");a.floatingWhatsApp({phone:a.getAttribute("data-phone"),popupMessage:a.getAttribute("data-popupmessage"),headerName:a.getAttribute("data-headername"),headerTitle:a.getAttribute("data-headertitle"),message:a.getAttribute("data-message"),placeholder:a.getAttribute("data-placeholder"),showPopup:a.getAttribute("data-showpopup"),position:a.getAttribute("data-position"),headerColor:a.getAttribute("data-headerColor"),
+autoOpenTimeout:a.getAttribute("data-autoOpenTimeout"),size:a.getAttribute("data-size"),buttonImage:a.getAttribute("data-image"),backgroundColor:a.getAttribute("data-backgroundColor")})});
